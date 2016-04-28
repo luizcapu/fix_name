1f43af4353bff4acc9cef7d7435ba8d8a313a69d
luizcapu <luizcapu@gmail.com>
2016-04-28 09:51:20 -0300

    copy cancellation date

--- src/main/java/br/adtsahring/com/fix_name/App.java
+++ src/main/java/br/adtsahring/com/fix_name/App.java
@@ -17,19 +17,15 @@ public class App
 	private static final String DB_ADTS_USER = "DB_ADTS_USER";
 	private static final String DB_ADTS_PASS = "DB_ADTS_PASS";
 	
-	private static final Integer qtRecords = 10000;
-	private static final String SEL_ADTS_USERS = "select * from tb_adt_user order by uid limit ?,"+qtRecords; 
-	private static PreparedStatement selAdtsUsers;
+	private static final String UPD_ADTS_USER = "update tb_adt_user set cancellation=? where uid_client=?"; 
+	private static PreparedStatement updAdtsUser;
 
 	private static Connection odinConn;	
 	private static final String DB_ODIN_JDBC_URL = "DB_ODIN_JDBC_URL";
 	private static final String DB_ODIN_USER = "DB_ODIN_USER";
 	private static final String DB_ODIN_PASS = "DB_ODIN_PASS";
 	
-	private static final String UPD_ODIN_USER = "update tb_user set ds_client = ? where uid_client = ?";
-	private static PreparedStatement updateOdinUser;
-
-	private static final String SEL_ODIN_USER = "select cancellation from tb_user where uid_client=?";
+	private static final String SEL_ODIN_USER = "select uid_client, cancellation from tb_user where cancellation is not null";
 	private static PreparedStatement selectOdinUser;
 	
     public static void main( String[] args ) throws SQLException
@@ -52,51 +48,30 @@ public class App
         		System.getenv(DB_ADTS_JDBC_URL), 
         		System.getenv(DB_ADTS_USER), 
         		System.getenv(DB_ADTS_PASS));
-        selAdtsUsers = adtsConn.prepareStatement(SEL_ADTS_USERS);
+        updAdtsUser = adtsConn.prepareStatement(UPD_ADTS_USER);
     	//-- END ADTS CONN
         
     	//-- BEGIN ODIN CONN
         odinConn = DriverManager.getConnection(
         		System.getenv(DB_ODIN_JDBC_URL), 
         		System.getenv(DB_ODIN_USER), 
-        		System.getenv(DB_ODIN_PASS));
-        
-        updateOdinUser = odinConn.prepareStatement(UPD_ODIN_USER);
+        		System.getenv(DB_ODIN_PASS));        
         selectOdinUser = odinConn.prepareStatement(SEL_ODIN_USER);
     	//-- END ODIN CONN
     }
     
     private void run() throws SQLException {
-    	
-    	Integer from = 0;
-    	boolean hasRecords = true;
-    	while (hasRecords) {
-    		
-    		hasRecords = false;
-    		
-    		selAdtsUsers.setInt(1, from);
-    		ResultSet adtsUsers = selAdtsUsers.executeQuery();
-    		
-    		try {
-        		while (adtsUsers.next()) {
-        			hasRecords = true;
-        			
-        			//User user = new User(adtsUsers);
-        			Long uidClient = adtsUsers.getLong("uid_client");
-        			String dsClient = adtsUsers.getString("ds_client");
-        			
-        			logger.info("Updating name to '"+dsClient+"' for uid client " + uidClient);
-        			
-        			updateOdinUser.setString(1, dsClient);
-        			updateOdinUser.setLong(2, uidClient);
-        			updateOdinUser.executeUpdate();
-        		}    		    			
-    		} finally {
-    			adtsUsers.close();
-    		}
-    		
-    		from += qtRecords;
-    	}
+		ResultSet odinUsers = selectOdinUser.executeQuery();		
+		try {
+    		while (odinUsers.next()) {
+    			logger.info("Updating cancellation to " + odinUsers.getTimestamp("cancellation") + " for user " + odinUsers.getInt("uid_client"));
+    			updAdtsUser.setTimestamp(1, odinUsers.getTimestamp("cancellation"));
+    			updAdtsUser.setInt(2, odinUsers.getInt("uid_client"));
+    			updAdtsUser.executeUpdate();
+    		}    		    			
+		} finally {
+			odinUsers.close();
+		}    	
     }
     
 }
